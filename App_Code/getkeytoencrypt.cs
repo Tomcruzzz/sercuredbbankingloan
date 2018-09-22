d628e32b940d0cb2a29572493fa8eff64d10fd92
Tom Antony <tomantony1997@gmail.com>
2018-09-22 20:45:32 +0530

    bank loan
    we know people whontakes loan from one bank and then move to another bank for loan without ending the first loan.
    this system checks all banks databases for possible loan theft,

---
+++ App_Code/getkeytoencrypt.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Security.Cryptography;
+using System.Text;
+
+/// <summary>
+/// Summary description for getkeytoencrypt
+/// </summary>
+public class getkeytoencrypt
+{
+    public static string GetUniqueKey()
+    {
+        int maxSize = 8;
+        //int minSize = 5 ;
+        char[] chars = new char[62];
+        string a;
+        a = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+        chars = a.ToCharArray();
+        int size = maxSize;
+        byte[] data = new byte[1];
+        RNGCryptoServiceProvider crypto = new RNGCryptoServiceProvider();
+        crypto.GetNonZeroBytes(data);
+        size = maxSize;
+        data = new byte[size];
+        crypto.GetNonZeroBytes(data);
+        StringBuilder result = new StringBuilder(size);
+        foreach (byte b in data)
+        {
+            result.Append(chars[b % (chars.Length)]);
+
+        }
+
+        // MessageBox.Show(r);
+        return result.ToString();
+    }
+}
